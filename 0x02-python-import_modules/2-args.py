6a5c43b68f4a826cb2f289dd3b48a5b90c52da29
Joseun <ologanj@gmail.com>
2021-11-18 14:58:05 +0000

    Python - import & modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         print('')
     else:
         print("{:d} arguments:".format(len(sys.argv) - 1), end="")
-        print('')    
+        print('')
         for i in range(1, len(sys.argv)):
             if i < len(sys.argv):
                 print("{:d}: {:s}".format(i, sys.argv[i]), end="")
