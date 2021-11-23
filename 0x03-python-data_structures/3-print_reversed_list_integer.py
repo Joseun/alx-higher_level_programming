fabb283ade0e163768275c5f38173921fbf1fece
Joseun <ologanj@gmail.com>
2021-11-23 10:55:19 +0000

    Corrections

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    n = len(my_list)
-    for i in range((n - 1), 0, -1):
-        print("{:d}".format(my_list[i]))
-    print("{:d}".format(my_list[0]))
+    if my_list == None:
+        return
+    n = len(my_list) - 1
+    if n == 0:
+        return
+    else:
+        for i in range(n, 0, -1):
+            print("{:d}".format(my_list[i]))
+        print("{:d}".format(my_list[0]))
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,8 +6,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     for i in range(2):
         if n > i:
             new_tuple[i] += tuple_a[i]
-        else: 0
-        if m > i:
-            new_tuple[i] += tuple_b[i]
-        else: 0
-    return tuple(new_tuple)
+            elif m > i:
+                new_tuple[i] += tuple_b[i]
+        return tuple(new_tuple)
