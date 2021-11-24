407a0969b3b611034bf2c938b981540da8539b80
Joseun <ologanj@gmail.com>
2021-11-24 17:39:05 +0000

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
-    if search != "":
+    if search != None:
         for i, j in enumerate(my_list):
             if j == search:
                 new_list[i] = replace 
