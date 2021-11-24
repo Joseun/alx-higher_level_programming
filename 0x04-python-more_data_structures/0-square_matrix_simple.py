2240588fe8e7c72cd7a58234e669fdfca1c5aa4e
Joseun <ologanj@gmail.com>
2021-11-24 17:35:58 +0000

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-'''using list comprehension to square contatents of a matrix'''
     if len(matrix) > 0:
         new_matrix = [[i*i for i in j] for j in matrix]
         return new_matrix
