72c01d0f784ce6119d3f31b6dffde1a9a2c5850e
Joseun <ologanj@gmail.com>
2021-11-24 19:05:19 +0000

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is None or len(a_dictionary) == 0:
-        return None
+        return "None"
     n = list(a_dictionary)
     best_key = n[0]
     if len(n) > 1:
