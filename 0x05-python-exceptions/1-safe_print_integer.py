38ca874f9590385f7149119a06ea2f06509e90b7
Joseun <ologanj@gmail.com>
2021-12-06 22:02:51 +0000

    Python Exceptions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,6 +4,6 @@ def safe_print_integer(value):
         print("{:d}".format(value), end="")
         print("")
         return True
-    except ValueError:
+    except (ValueError, TypeError):
             return False
     print("")
