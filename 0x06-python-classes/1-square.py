16f7667282b6efadbcc718a4eee4dd7d10835528
Joseun <ologanj@gmail.com>
2021-12-10 00:31:32 +0000

    Python - Classes and Objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Creating the class Square"""
+
+
+class Square:
+    """This is an empty class that defines a square"""
+
+    def __init__(self, size):
+        """This is an empty class that defines a square of size 'size
+
+        Args:
+            size: paramter to determine the size of the square.
+
+        """
+
+        self.__size = size  #: Size of the square.
