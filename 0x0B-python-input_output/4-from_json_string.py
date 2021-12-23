10ce3035b33dc4a2652fb7af94f80a74f32c3f60
Joseun <ologanj@gmail.com>
2021-12-23 00:58:42 +0000

    Python - Input/Output

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""JSON reconversion to objects"""
+
+
+import json
+
+def from_json_string(my_str):
+    """A function that returns a JSON representation of an object back to
+    the original format
+
+    Args:
+        my_str (:obj:str): object to be converted
+.
+    Returns:
+        the object.
+
+    """
+
+    return json.loads(my_str)
