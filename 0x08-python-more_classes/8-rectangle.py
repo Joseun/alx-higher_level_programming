68d3cbc1daebd45c2efecd3cce6542f29fa89688
Joseun <ologanj@gmail.com>
2022-01-22 09:36:19 +0000

    Corrections

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -28,7 +28,7 @@ class Rectangle:
         elif height < 0:
             raise ValueError('height must be >= 0')
         self.__height = height
-        Rectangle.number_of_instances+=1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -101,7 +101,7 @@ class Rectangle:
         Prompts an instance being deleted
         """
 
-        Rectangle.number_of_instances-=1
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
     @staticmethod
