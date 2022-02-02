990f8206cdd055d9aa1ec482282f54318e8b4acb
Joseun <ologanj@gmail.com>
2022-02-02 00:43:09 +0000

    SQL - Introduction

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- Inserts a new row in a table in the database on the MySQL server.
-INSERT INTO first_table (id INT, name VARCHAR(256)
+INSERT INTO `first_table` (id INT, name VARCHAR(256))
 VALUES (89, 'Best School');
