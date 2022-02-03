469c08e6f7a5a8bd57bfdb70f4ddefcb6e3301af
Joseun <ologanj@gmail.com>
2022-02-03 02:49:41 +0000

    SQL - More queries

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- Lists all shows in hbtn_0d_tvshows that have at least one genre linked.
 -- Records are sorted by ascending tv_shows.title and tv_show_genres.genre_id.
-SELECT `tv_genres`.`name` AS genre, COUNT(`tv_shows_genres`.`genre_id`) AS number_of_shows
-FROM `tv_genres`
-RIGHT JOIN `tv_show_genres`
-ON `tv_genres`.`id` = `tv_show_genres`.`genre_id`
-GROUP BY genre
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_genres
+RIGHT JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_show_genres.genre_id IS NOT NULL
 ORDER BY number_of_shows DESC;
