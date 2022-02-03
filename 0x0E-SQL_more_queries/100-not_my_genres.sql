01c6a70e5daa08d7fae407f4c59f3d9e9af24d82
Joseun <ologanj@gmail.com>
2022-02-03 03:01:50 +0000

    SQL - More queries

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,14 @@
+-- Lists all shows in hbtn_0d_tvshows that have at least one genre linked.
+-- Records are sorted by ascending tv_shows.title and tv_show_genres.genre_id.
+SELECT `tv_genres`.`name`
+FROM `tv_genres`
+INNER JOIN `tv_show_genres`
+      ON `tv_show_genres`.`genre_id` = `tv_genres`.`id`
+INNER JOIN `tv_shows`
+      ON `tv_shows`.`id` = `tv_show_genres`.`show_id`
+WHERE name NOT IN (
+	      SELECT (*)
+	      FROM `tv_shows`
+	      WHERE `tv_shows`.`title` = 'Dexter'
+	      )
+ORDER BY `tv_genres`.`name`;
