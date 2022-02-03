55b9ebfc8f49e2ba6c6bc3629295d3a0629c3411
Joseun <ologanj@gmail.com>
2022-02-03 02:27:48 +0000

    SQL - More queries

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 -- Records are sorted by ascending tv_shows.title and tv_show_genres.genre_id.
 SELECT `tv_genres`.`name` AS genre, COUNT(`tv_shows_genres`.`genre_id`) AS number_of_shows
 FROM `tv_genres`
-INNER JOIN `tv_show_genres`
+RIGHT JOIN `tv_show_genres`
 ON `tv_genres`.`id` = `tv_show_genres`.`genre_id`
 GROUP BY genre
 ORDER BY number_of_shows DESC;
