ebe41fb755d4024df72a9b4332fbc9d73df2d764
Joseun <ologanj@gmail.com>
2022-02-03 01:41:16 +0000

    SQL - More queries

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- Lists all shows in hbtn_0d_tvshows that have at least one genre linked.
+-- Records are sorted by ascending tv_shows.title and tv_show_genres.genre_id.
+SELECT `tv_shows`.`title`, `tv_genres`.`name`
+FROM `tv_shows`
+LEFT JOIN `tv_show_genres`
+      ON  `tv_show_genres`.`show_id` = `tv_shows`.`id`
+LEFT JOIN `tv_genres`
+      ON `tv_show_genres`.`genre_id` = `tv_genres`.`id`
+ORDER BY `tv_shows`.`title`, `tv_genres`.`name`;
