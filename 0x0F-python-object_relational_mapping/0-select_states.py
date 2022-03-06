817b467d4b274f2682743a7ad7ae8ce9865bba7f
Joseun <ologanj@gmail.com>
2022-03-06 19:08:36 +0000

    Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 		port=3306,
 		user=argv[1],
 		password=argv[2],
-		database=argv[3],
+		database=argv[3]
 	)
 	cur = conn.cursor()
 	cur.execute("SELECT * FROM states ORDER BY id ASC")
