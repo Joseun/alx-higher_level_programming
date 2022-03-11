077e3a346d65f5b72f4e17310f69f36db9c61e71
Joseun <ologanj@gmail.com>
2022-03-11 21:52:20 +0000

    Corrections

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" 
+"""
 a script that lists all states with a name starting
-with N (upper N) from the database hbtn_0e_0_usa 
+with N (upper N) from the database hbtn_0e_0_usa
 """
 
 
@@ -9,11 +9,11 @@ if __name__ == "__main__":
     import MySQLdb
     from sys import argv
     db = MySQLdb.connect(
-        host = "localhost",
-        port = 3306,
-        user = argv[1],
-        password = argv[2],
-        database = argv[3]
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        database=argv[3]
     )
     cur = db.cursor()
     cur.execute("SELECT *\
