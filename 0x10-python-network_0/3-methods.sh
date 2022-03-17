af23e222e7ffdd2fab73b30d640934bb317dc1d6
Joseun <ologanj@gmail.com>
2022-03-17 22:39:43 +0000

    Python - Network

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sI -X OPTIONS "$1" #| awk '/Allow/{print $2}'
+curl -si -X OPTIONS "$1" | awk '/Allow/{print $0}' | cut -d ' ' -f2-
