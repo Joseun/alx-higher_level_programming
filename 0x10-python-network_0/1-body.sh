939b952edee1fdcdfe74a96c62cb4e74a0b95b01
Joseun <ologanj@gmail.com>
2022-03-17 22:00:32 +0000

    Python - Network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a GET request to the URL, and displays the body of the response# script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-SC="$(curl -s -i  "$1"| head -1 | awk '{print $2}')"
-if [ "$SC" -eq 200 ]
-    then
-        "$(curl -sL "$1")"
-fi
+curl -sL "$1"
